c16302249ce26ef38a75c3754729cc4d97864723
Mohd Fahmi Mohd Shari <estudiantes678@gmail.com>
2018-11-30 10:53:58 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
     'facebook' => [
         'client_id' => '295496244376169',
         'client_secret' => 'bcdc74969f0d92a73fb7f853aef7732f',
-        'redirect' => 'https://employee.adnexio.my/callback',
+        'redirect' => 'https://career.adnexio.my/callback',
     ],
 	
 	/*
@@ -52,7 +52,7 @@ return [
     'google' => [
         'client_id' => '168357464964-ahqgllohfl1q15cqucjt6vguon1i73q9.apps.googleusercontent.com',
         'client_secret' => 'VGrJdLa6dXUsS3sRTcudOEKP',
-        'redirect' => 'https://employee.adnexio.my/gcallback',
+        'redirect' => 'https://career.adnexio.my/gcallback',
 	],
 		
 	/*	
