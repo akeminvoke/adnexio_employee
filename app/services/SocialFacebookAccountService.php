e37900dae100c98870a4a314c57bb46499281dc8
Mohd Fahmi Mohd Shari <estudiantes678@gmail.com>
2018-11-30 11:36:58 +0800

    Update SocialFacebookAccountService.php

--- app/services/SocialFacebookAccountService.php
+++ app/services/SocialFacebookAccountService.php
@@ -42,7 +42,7 @@ class SocialFacebookAccountService
                     'email' => $providerUser->getEmail(),
                     'name' => $providerUser->getName(),
                     'password' => md5(rand(1,10000)),
-                    'avatar'=>  $profileavatar,
+                    'profile_images'=>  $profileavatar,
                 ]);
             }
 
